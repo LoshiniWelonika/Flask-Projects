07849c8424f4f8438cc4a683351ca76f5da8cd40
LoshiniWelonika <welonikakedapanawala@gmail.com>
2025-09-26 16:51:07 +0530

    Creating an API KEy

---
+++ Flask_Bootstrap/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, url_for,request
+import requests 
+from config import NEWS_API_KEY
+
+
+#Create a flask app
+app = Flask(__name__)
+
+
+#Homepage
+@app.route("/")
+def index():
+    query = request.args.get("query", "latest")
+    url = f"https://newsapi.org/v2/everything?q={query}&apiKey={NEWS_API_KEY}"
+    response = requests.get(url) 
+    news_data = response.json() 
+    print(news_data)
+
+    return "Testing"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True) 
