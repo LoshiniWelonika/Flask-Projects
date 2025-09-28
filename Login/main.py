55f23548ff645a72f98eba9eb8eff0efd33734eb
LoshiniWelonika <welonikakedapanawala@gmail.com>
2025-09-28 12:38:49 +0530

    Creating Databaase Model

--- Login/main.py
+++ Login/main.py
@@ -12,16 +12,32 @@ db = SQLAlchemy(app)
 
 
 #Database Model
+class User(db.Model):
+    #Class Variables
+    id = db.column(db.Integer, primary_key=True)
+    username = db.column(db.String(25), unique=True, nullable=False)
+    password = db.column(db.String(15), primary_key=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+  
 
 
 
-
-#Routes
+#_______________________Routes_________________________
 @app.route("/")
 def home():
     if "username" in session:
         return redirect(url_for('dashboard'))
     return render_template("index.html")
+
+
+
+
+
 
 
 
