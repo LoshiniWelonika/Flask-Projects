b9a0831948943271e6c94aa0ad07a2e465f02b15
LoshiniWelonika <welonikakedapanawala@gmail.com>
2025-09-28 20:39:40 +0530

    Register, Logout, Dashboard routes

--- Login/main.py
+++ Login/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, url_for
 from werkzeug.security import generate_password_hash, check_password_hash 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -7,16 +7,16 @@ app.secret_key = "your_secret_key"
 
 #Configure SQLAlchemy
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
-app.config["SQLALCHMEY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app) 
 
 
 #Database Model
 class User(db.Model):
     #Class Variables
-    id = db.column(db.Integer, primary_key=True)
-    username = db.column(db.String(25), unique=True, nullable=False)
-    password = db.column(db.String(15), primary_key=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    password_hash = db.Column(db.String(150), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -30,13 +30,12 @@ class User(db.Model):
 #_______________________Routes_________________________
 @app.route("/")
 def home():
-    if "username" in session:
-        return redirect(url_for('dashboard'))
     return render_template("index.html")
 
-@app.route("/login", methods="POST")
+
+
+@app.route("/login", methods=["POST"])
 def login():
-    #Collect info from the form
     username = request.form["username"]
     password = request.form["password"]
     user = User.query.filter_by(username=username).first() 
@@ -46,18 +45,40 @@ def login():
         return redirect(url_for('dashboard'))
     else:
         return render_template("index.html")
-
-
-    #Check if the details in the db
-
-
-    #Otherwise show the home page
+    
 
 
 
+@app.route("/register", methods=["POST"])
+def register():
+    username = request.form["username"]
+    password = request.form["password"]
+    user = User.query.filter_by(username=username).first() 
+    if user:
+        return render_template("index.html", error="User already exists")
+    else:
+        new_user = User(username=username)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+        session['username'] = username
+        return redirect(url_for('dashboard'))
+
+@app.route("/dashboard")
+def dashboard():
+    if 'username' in session:
+        return render_template("dashboard.html", username=session['username'])
+    else:
+        return redirect(url_for('home'))
+    
+@app.route("/logout")
+def logout():
+    session.pop('usename', None)
+    return redirect(url_for('home')) 
 
 
 
+#Run the app
 if __name__ == "__main__":
     with app.app_context():
         db.create_all() 
