8e6073f02f6aa0771ebd9fc911f52290943e0356
LoshiniWelonika <welonikakedapanawala@gmail.com>
2025-10-12 15:33:06 +0530

    Logging and Config

--- Chat App/main.py
+++ Chat App/main.py
@@ -10,7 +10,41 @@ from flask_socketio import SocketIO, emit, join_room, leave_room
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 
+#Logging
+logging.basicConfig(
+    level=logging. INFO,
+    format= '%(asctime)s - %(name)s - %(levelname)s -%(message)s'
+)
+
+logger = logging.getLogger(__name__)
+
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY") or os.urandom(24)
+    DEBUG = os.environ.get("FLASK_DEBUG", "False"). lower() in ('true', "1", "t")
+    CORS_ORIGINS = os.environ.get("CORS_ORIGINS", "*")
+
+    #Chat Rooms
+    CHAT_ROOMS = [
+        "General",
+        "Special",
+        "Friends",
+        "Official" 
+    ]
+
+
+
 app = Flask(__name__)
+app.config.from_object(Config)
+
+
+#Handle reverse proxy
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
+
+
+
+
 
 @app.route("/")
 def index():
